00876e22178c0ad01750ffcc7767c00e27924286
Ram <Dev1_git@gmail.com>
2022-03-09 12:18:51 +0530

    first commit

---
+++ src/test/java/demo9/org/demo9/AppTest.java
@@ -0,0 +1,20 @@
+package demo9.org.demo9;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
